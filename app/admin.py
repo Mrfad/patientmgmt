21d06eb9f723f27158ee75685b8e10ef65e9165d
mrfad <fady.ghalayiny@outlook.com>
2022-04-12 15:45:31 +0300

    finished lesson 2

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+
+class patinetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'gender', 'created_at']
+    search_fields = ['name', 'phone', 'email', 'gender']
+    list_per_page = 8
+    
+admin.site.register(Patient, patinetAdmin)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Patient
 
 # function to render the home page
 def frontend(request):
@@ -14,4 +17,18 @@ def backend(request):
 # function to add patient
 @login_required(login_url="login")
 def add_patient(request):
-    return render(request, 'app/add.html')
+    if request.method == 'POST':
+        if request.POST.get('name') and request.POST.get('phone') and request.POST.get('email') and request.POST.get('age') and request.POST.get('gender') or request.POST.get('note'):
+            patient = Patient()
+            patient.name = request.POST.get('name')
+            patient.phone = request.POST.get('phone')
+            patient.email = request.POST.get('email')
+            patient.age = request.POST.get('age')
+            patient.gender = request.POST.get('gender')
+            patient.note = request.POST.get('note')
+            patient.save()
+            messages.success(request, 'Patient added successfully')
+            return HttpResponseRedirect('/backend')
+    else:
+        return render(request, "app/add.html")
+            
